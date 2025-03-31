287616b8ebfc859a4aa2cd43ba058b7b8ba2fc81
JSimko4 <xsimkoj2@stuba.sk>
2025-03-31 15:15:50 +0200

    hello world app

---
+++ src/components/sarsabsim-app/sarsabsim-app.tsx
@@ -0,0 +1,17 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'sarsabsim-app',
+  styleUrl: 'sarsabsim-app.css',
+  shadow: true,
+})
+export class SarsabsimApp {
+  render() {
+    return (
+      <Host>
+        <span>Hello world - Saraka Sabol Šimko </span>
+        <span>Ak to nefunguje tak produkčný cluster zhodil určite Patrik Sabol</span>
+      </Host>
+    );
+  }
+}
